87970828e6797c7b7e39c87b05ad700f0cfa08bc
Sinan SALIH <snnslh@outlook.com>
2019-10-23 01:37:32 +0300

    clear repository

--- assets/js/page.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 10) {
-       document.getElementById('navbar').classList.add('shadow');
-    }
-    else {
-        document.getElementById('navbar').classList.remove('shadow');
-    }
-});
